86c1e0b651421f4302579eefaa6e9fa012f2eff5
juyongjiang <csjuyongjiang@gmail.com>
2022-01-04 19:28:22 +0900

    upgrade

--- model/SEED_Encoder/__init__.py
+++
@@ -1,3 +0,0 @@
-from model.SEED_Encoder.tokenization_seed_encoder import *
-from model.SEED_Encoder.configuration_seed_encoder import *
-from model.SEED_Encoder.modeling_seed_encoder import *
--- model/models.py
+++ model/models.py
@@ -13,8 +13,6 @@ from transformers import (
 )
 import torch.nn.functional as F
 from data.process_fn import triple_process_fn, triple2dual_process_fn
-from model.SEED_Encoder import SEEDEncoderConfig, SEEDTokenizer, SEEDEncoderForSequenceClassification,SEEDEncoderForMaskedLM
-
 
 class EmbeddingMixin:
     """
@@ -152,28 +150,6 @@ class RobertaDot_CLF_ANN_NLL_MultiChunk(NLL_MultiChunk, RobertaDot_NLL_LN):
 
         return complex_emb_k  # size [batchS, chunk_factor, embeddingS]
 
-class SEEDEncoderDot_NLL_LN(NLL, SEEDEncoderForSequenceClassification):
-    """None
-    Compress embedding to 200d, then computes NLL loss.
-    """
-    def __init__(self, config, model_argobj=None):
-        NLL.__init__(self, model_argobj)
-        SEEDEncoderForSequenceClassification.__init__(self, config)
-        self.embeddingHead = nn.Linear(config.encoder_embed_dim, 768)
-        self.norm = nn.LayerNorm(768)
-        self.apply(self._init_weights)
-
-    def query_emb(self, input_ids, attention_mask=None):
-        outputs1 = self.seed_encoder.encoder(input_ids)
-
-        full_emb = self.masked_mean_or_first(outputs1, attention_mask)
-        query1 = self.norm(self.embeddingHead(full_emb))
-
-        return query1
-
-    def body_emb(self, input_ids, attention_mask=None):
-        return self.query_emb(input_ids, attention_mask)
-
 class HFBertEncoder(BertModel):
     def __init__(self, config):
         BertModel.__init__(self, config)
@@ -242,7 +218,6 @@ configs = [
     MSMarcoConfig(name="rdot_nll", model=RobertaDot_NLL_LN, use_mean=False,),
     MSMarcoConfig(name="rdot_nll_multi_chunk", model=RobertaDot_CLF_ANN_NLL_MultiChunk, use_mean=False,),
     MSMarcoConfig(name="dpr", model=BiEncoder, tokenizer_class=BertTokenizer, config_class=BertConfig, use_mean=False,),
-    MSMarcoConfig(name="seeddot_nll", model=SEEDEncoderDot_NLL_LN, use_mean=False, tokenizer_class=SEEDTokenizer, config_class=SEEDEncoderConfig,),
 ]
 
 MSMarcoConfigDict = {cfg.name: cfg for cfg in configs}
