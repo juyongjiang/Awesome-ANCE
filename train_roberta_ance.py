5af3102723aba9b5bf31d9a2104189ce2dd4187d
juyongjiang <csjuyongjiang@gmail.com>
2022-01-06 17:42:44 +0900

    upgrade

--- train_roberta_ance.py
+++ train_roberta_ance.py
@@ -279,45 +279,6 @@ def train(args, model, tokenizer, query_cache, passage_cache):
 
     return global_step
 
-def set_env(args):
-    # Setup distant debugging if needed
-    if args.server_ip and args.server_port:
-        # Distant debugging - see
-        # https://code.visualstudio.com/docs/python/debugging#_attach-to-a-local-script
-        import ptvsd
-
-        print("Waiting for debugger attach")
-        ptvsd.enable_attach(address=(args.server_ip, args.server_port), redirect_output=True)
-        ptvsd.wait_for_attach()
-
-    # Setup CUDA, GPU & distributed training
-    if args.local_rank == -1 or args.no_cuda:
-        device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
-        args.n_gpu = torch.cuda.device_count()
-    else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
-        torch.cuda.set_device(args.local_rank)
-        device = torch.device("cuda", args.local_rank)
-        torch.distributed.init_process_group(backend="nccl")
-        args.n_gpu = 1
-    args.device = device
-
-    # Setup logging
-    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s", 
-                        datefmt="%m/%d/%Y %H:%M:%S", 
-                        level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN,)
-    logger.warning(
-                    "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
-                    args.local_rank,
-                    device,
-                    args.n_gpu,
-                    bool(args.local_rank != -1),
-                    args.fp16,
-    )
-
-    # Set seed
-    set_seed(args)
-
-
 def load_model(args):
     # Prepare GLUE task
     args.task_name = args.task_name.lower()
@@ -365,7 +326,6 @@ def load_model(args):
 
     return tokenizer, model
 
-
 def save_checkpoint(args, model, tokenizer):
     # Saving best-practices: if you use defaults names for the model, you can
     # reload it using from_pretrained()
@@ -388,6 +348,43 @@ def save_checkpoint(args, model, tokenizer):
     if args.local_rank != -1:
         dist.barrier()
 
+def set_env(args):
+    # Setup distant debugging if needed
+    if args.server_ip and args.server_port:
+        # Distant debugging - see
+        # https://code.visualstudio.com/docs/python/debugging#_attach-to-a-local-script
+        import ptvsd
+
+        print("Waiting for debugger attach")
+        ptvsd.enable_attach(address=(args.server_ip, args.server_port), redirect_output=True)
+        ptvsd.wait_for_attach()
+
+    # Setup CUDA, GPU & distributed training
+    if args.local_rank == -1 or args.no_cuda:
+        device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+        args.n_gpu = torch.cuda.device_count()
+    else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+        torch.cuda.set_device(args.local_rank)
+        device = torch.device("cuda", args.local_rank)
+        torch.distributed.init_process_group(backend="nccl")
+        args.n_gpu = 1
+    args.device = device
+
+    # Setup logging
+    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s", 
+                        datefmt="%m/%d/%Y %H:%M:%S", 
+                        level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN,)
+    logger.warning(
+                    "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+                    args.local_rank,
+                    device,
+                    args.n_gpu,
+                    bool(args.local_rank != -1),
+                    args.fp16,
+    )
+
+    # Set seed
+    set_seed(args)
 
 def main():
     parser = argparse.ArgumentParser()
